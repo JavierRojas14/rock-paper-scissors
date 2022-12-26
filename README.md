4dd8f1d7475bde6eb728b105edd5fb96357909a0
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-12-26 14:43:40 -0300

    Add function to play a round

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rock-paper-scissors
 This is the repository for the project of "Rock Paper &amp; Scissors" for The Odin Project.
+
+This project consist of these functions:
+
+getComputerChoice: 
--- script.js
+++ script.js
@@ -12,5 +12,52 @@ function getIndexToChoose(array) {
     return index
 }
 
-// let choice = getComputerChoice();
-// console.log(choice);
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionLower = playerSelection.toLowerCase();
+    let computerSelectionLower = computerSelection.toLowerCase();
+
+    if (playerSelectionLower === 'rock') {
+        if (computerSelectionLower === 'rock') {
+            result = "Well, that's a draw";
+
+        } else if (computerSelectionLower === 'paper') {
+            result = 'You Lose! Paper beats Rock';
+
+        } else if (computerSelectionLower === 'scissors') {
+            result = 'You Win! Rock beats Scissors';
+
+        }
+
+    } else if (playerSelectionLower === 'paper') {
+        if (computerSelectionLower === 'rock') {
+            result = 'You Win! Paper beats Rock';
+
+        } else if (computerSelectionLower === 'paper') {
+            result = "Well, that's a draw";
+
+        } else if (computerSelectionLower === 'scissors') {
+            result = 'You Lose! Scissors beats Paper';
+        }
+
+    } else if ((playerSelectionLower === 'scissors')) {
+        if (computerSelectionLower === 'rock') {
+            result = 'You Lose! Rock beats Scissors';
+
+        } else if (computerSelectionLower === 'paper') {
+            result = 'You Win! Scissors beats Paper';
+
+        } else if (computerSelectionLower === 'scissors') {
+            result = "Well, that's a draw";
+        }
+
+    } else {
+        result = 'Sorry, that is not a valid option';
+    }
+
+    return result
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
