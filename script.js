4cc0a9e60fbf77e8c94da8738e9a76d4c7c9e8ed
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-01-05 13:35:53 -0300

    Add event listeners to buttons of decisions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    const decisions = ['Rock', 'Paper', 'Scissors'];
+    const decisions = ['rock', 'paper', 'scissors'];
     let randomIndex = getIndexToChoose(decisions);
     let randomChoice = decisions[randomIndex]
 
@@ -13,9 +13,9 @@ function getIndexToChoose(array) {
 }
 
 
-function playRound(playerSelection, computerSelection) {
-    let playerSelectionLower = playerSelection.toLowerCase();
-    let computerSelectionLower = computerSelection.toLowerCase();
+function playRound(event) {
+    const playerSelectionLower = event.target.classList.value;
+    const computerSelectionLower = getComputerChoice();
 
     if (playerSelectionLower === 'rock') {
         if (computerSelectionLower === 'rock') {
@@ -58,10 +58,25 @@ function playRound(playerSelection, computerSelection) {
     return result
 }
 
+
+
 function game() {
     let playerSelection = prompt('What is your choice?');
     let result = playRound(playerSelection, getComputerChoice());
     console.log(result);
 }
 
-game();
+
+//  game();
+
+// Primero se seleccciona la opcion del jugador. Esto se hace con un event listener.
+// O sea, cada vez que se clickee un boton, se tiene que obtener la decision del jugador
+// 1. Agregar event listeners a los botones
+// 2. Cada vez que se clickee un boton llamar a la funcion playRound
+// 3. La seleccion del jugador va a ser la classList.value del jugador
+// 4. La seleccion de la maquina se obtiene con la funcion getComputerChoice()
+
+const decisionsNodes = document.querySelectorAll('button');
+decisionsNodes.forEach(button => {
+    button.addEventListener('click', playRound)
+});
