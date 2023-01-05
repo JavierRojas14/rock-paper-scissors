2b1a9a378d0826ec45bca7e768df29e540a60ea5
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-01-05 13:58:47 -0300

    Linter

--- script.js
+++ script.js
@@ -1,82 +1,65 @@
 function getComputerChoice() {
-    const decisions = ['rock', 'paper', 'scissors'];
+    const decisions = ["rock", "paper", "scissors"];
     let randomIndex = getIndexToChoose(decisions);
-    let randomChoice = decisions[randomIndex]
+    let randomChoice = decisions[randomIndex];
 
-    return randomChoice
-
+    return randomChoice;
 }
 
 function getIndexToChoose(array) {
     let index = Math.floor(Math.random() * array.length);
-    return index
+    return index;
 }
 
-
-function playRound(event) {
-    const playerSelectionLower = event.target.classList.value;
-    const computerSelectionLower = getComputerChoice();
-
-    if (playerSelectionLower === 'rock') {
-        if (computerSelectionLower === 'rock') {
+function decideWinnerOfRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
             result = "Well, that's a draw";
-
-        } else if (computerSelectionLower === 'paper') {
-            result = 'You Lose! Paper beats Rock';
-
-        } else if (computerSelectionLower === 'scissors') {
-            result = 'You Win! Rock beats Scissors';
-
+        } else if (computerSelection === "paper") {
+            result = "You Lose! Paper beats Rock";
+        } else if (computerSelection === "scissors") {
+            result = "You Win! Rock beats Scissors";
         }
-
-    } else if (playerSelectionLower === 'paper') {
-        if (computerSelectionLower === 'rock') {
-            result = 'You Win! Paper beats Rock';
-
-        } else if (computerSelectionLower === 'paper') {
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            result = "You Win! Paper beats Rock";
+        } else if (computerSelection === "paper") {
             result = "Well, that's a draw";
-
-        } else if (computerSelectionLower === 'scissors') {
-            result = 'You Lose! Scissors beats Paper';
+        } else if (computerSelection === "scissors") {
+            result = "You Lose! Scissors beats Paper";
         }
-
-    } else if ((playerSelectionLower === 'scissors')) {
-        if (computerSelectionLower === 'rock') {
-            result = 'You Lose! Rock beats Scissors';
-
-        } else if (computerSelectionLower === 'paper') {
-            result = 'You Win! Scissors beats Paper';
-
-        } else if (computerSelectionLower === 'scissors') {
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            result = "You Lose! Rock beats Scissors";
+        } else if (computerSelection === "paper") {
+            result = "You Win! Scissors beats Paper";
+        } else if (computerSelection === "scissors") {
             result = "Well, that's a draw";
         }
-
     } else {
-        result = 'Sorry, that is not a valid option';
+        result = "Sorry, that is not a valid option";
     }
 
-    return result
+    return result;
+}
+
+function showResult(result) {
+    const resultBox = document.querySelector(".results");
+    resultBox.textContent = result;
+}
+
+function playRound(event) {
+    const playerSelection = event.target.classList.value;
+    const computerSelection = getComputerChoice();
+
+    let result = decideWinnerOfRound(playerSelection, computerSelection);
+
+    showResult(result);
 }
 
 
 
-function game() {
-    let playerSelection = prompt('What is your choice?');
-    let result = playRound(playerSelection, getComputerChoice());
-    console.log(result);
-}
-
-
-//  game();
-
-// Primero se seleccciona la opcion del jugador. Esto se hace con un event listener.
-// O sea, cada vez que se clickee un boton, se tiene que obtener la decision del jugador
-// 1. Agregar event listeners a los botones
-// 2. Cada vez que se clickee un boton llamar a la funcion playRound
-// 3. La seleccion del jugador va a ser la classList.value del jugador
-// 4. La seleccion de la maquina se obtiene con la funcion getComputerChoice()
-
-const decisionsNodes = document.querySelectorAll('button');
-decisionsNodes.forEach(button => {
-    button.addEventListener('click', playRound)
+const decisionsNodes = document.querySelectorAll("button");
+decisionsNodes.forEach((button) => {
+    button.addEventListener("click", playRound);
 });
